5815f989991d3e3a03ae673c0298917581debe4e
TrStans606 <100236022+TrStans606@users.noreply.github.com>
2024-12-16 18:24:34 -0500

    Delete Programs/wcdest-source directory

--- Programs/wcdest-source/src/suffixcluster.h
+++
@@ -1,7 +0,0 @@
-
-
-int  suffix(WorkPtr work, int s1, int s2, int rcflag);
-
-void do_suffix_cluster (WorkPtr work);
-
-void do_kseed_suffixcluster(FILE *outf, WorkPtr work);
--- Programs/wcdest-source/src/wcd_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SEQFILE=$2
-OUTFILE=${SEQFILE}X.clt
-WCD=wcd
-COMP2EXT=comp2ext.pl
-$WCD --show_clusters $SEQFILE | $COMP2EXT >$OUTFILE
-
